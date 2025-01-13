1871d1a617a744a940468cee0c73e40a95604eef
Angel <34399672+Malak96@users.noreply.github.com>
2025-01-13 18:49:37 -0300

    YTMP3.bat

--- YTMP3.bat
+++ YTMP3.bat
@@ -2,10 +2,23 @@
 title YTMP3 - Actualización Automática
 
 :: Actualizar el repositorio
-git pull
+git pull > temp.txt
 
-:: Actualizar yt_dlp a la última versión
+:: Verificar si hubo cambios
+find "Updating" temp.txt > nul
+if %errorlevel% equ 0 (
+    echo Se detectaron cambios en el repositorio. Reiniciando...
+    timeout /t 3 /nobreak > nul
+    start "" "%~f0"
+    exit
+)
+
+:: Eliminar el archivo temporal
+del temp.txt
+
+:: Actualizar yt_dlp
 python -m pip install -U yt-dlp
 
 :: Ejecutar el script
 python YTMP3_v2.py
+
