64b229448db514019f809fd5b034edc6eb1b86bf
Angel <34399672+Malak96@users.noreply.github.com>
2025-01-22 22:28:40 -0300

    Update YTMP3.bat

--- YTMP3.bat
+++ YTMP3.bat
@@ -29,6 +29,7 @@ if not exist "%YT_DLP%" (
     curl -L -o yt-dlp.exe https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe
     timeout /t 3 /nobreak > nul
     start "" "%~f0"
+    exit
 )
 
 :: Actualizar yt-dlp a la última versión
